1d6360e8d5e1b00bd97bd1aeaefbcd2a770c860c
lupin-oomura <lupin.Oomura@gmail.com>
2022-09-05 02:28:10 +0900

    test

--- helloFlask.py
+++ helloFlask.py
@@ -9,9 +9,16 @@ def hello():
     return '<h1>Hello World</h1>'
 
 
-@app.route('/render')
+@app.route('/render', methods = ["GET" , "POST"])
 def index():
-    return render_template('render.html')
+
+   if request.method == 'POST':
+       user_id = request.form['user_id'] # formのname = "user_id"を取得
+       return render_template('render.html', user_id = user_id)
+   else:
+       return render_template('render.html')
+
+#    return render_template('render.html')
 
 
 if __name__ == '__main__':
